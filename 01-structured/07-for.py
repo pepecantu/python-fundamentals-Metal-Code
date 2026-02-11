5b8c409417407169ac2662d113be379eea982c8a
Jose A Cantu <jacantu@hotmail.com>
2026-02-11 06:42:05 -0600

    add 07-for.py

---
+++ 01-structured/07-for.py
@@ -0,0 +1,20 @@
+import os
+os.system('cls')
+
+for i in range(5):
+    print(i)
+ 
+print("-----------")
+    
+for i in range(1, 11):
+    print(i)
+
+print("-----------")
+
+for i in range(1, 11, 2):
+    print(i)
+    
+print("-----------")
+
+for i in range(0, 101, 10):
+    print(i)
