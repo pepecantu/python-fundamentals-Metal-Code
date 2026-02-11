78c1625f638ab2089a6063003ef9f60ab7668fc3
Jose A Cantu <jacantu@hotmail.com>
2026-02-11 06:40:10 -0600

    add 02-type.py

---
+++ 01-structured/02-type.py
@@ -0,0 +1,38 @@
+import os
+os.system("cls")
+
+number1 = 10
+number2 = 5
+
+result_add = number1 + number2
+result_mul = number1 * number2
+
+print(result_add)
+print(result_mul)
+
+decimal1 = 1.1
+decimal2 = 2.5
+
+result_add_decimal = decimal1 + decimal2
+
+print(result_add_decimal)
+
+name1 = "Juan"
+name2 = "Mendoza"
+
+full_name = name1 + " " + name2
+print(full_name)
+
+is_true = True
+print(is_true)
+
+major_age = 18
+age = 25
+is_major = age >= major_age
+
+print( is_major)
+
+print(type(number1))
+print(type(decimal1))
+print(type(name1))
+print(type(is_major))
