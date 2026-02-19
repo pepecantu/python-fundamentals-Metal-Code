f5d1781d310bf2f471683af3b360f41407c0b3be
Jose A Cantu <jacantu@hotmail.com>
2026-02-19 03:59:56 -0600

    add 01-tupla.py

---
+++ 03-collections/01-tuple.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls')
+
+numbers =(1,2,3,4,5)
+print(numbers[0])
+print(numbers[1])
+
+print(numbers[-1])
+
+# numbers[0] = 10
+data = ("Juan", 30, "Programador")
+
+print(data)
