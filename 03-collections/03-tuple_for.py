af360d11d40209c1e495fe03f81bf0da289216e2
Jose A Cantu <jacantu@hotmail.com>
2026-02-19 04:24:57 -0600

    add 03-tuple_for.py

---
+++ 03-collections/03-tuple_for.py
@@ -0,0 +1,23 @@
+import os
+os.system('cls')
+
+names = ("Ana", "Pedro", "Francisco", "Naomi")
+
+for name in names: # Recirriendo la tupla
+    print(name)
+    
+# imprimiendo las letras de name en forma individual
+name = "Jacinto"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+print("Iterando las letras de name")
+
+for letter in name:
+    print(letter)
